caaacdb4d84fd46417e7cf575bfff487be0eb5b7
andreaBitter <167077831+andreaBitter@users.noreply.github.com>
2025-06-13 11:29:03 +0200

    Create min.py für den kleinsten Wert

---
+++ app/analysis/min.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def compute(series: pd.Series) -> float:
+    """Compute the minimum of a pandas Series.
+
+    Args:
+        series (pd.Series): Input series to compute the minimum.
+
+    Returns:
+        float: The minimum of the series.
+
+    """
+    return series.min()
